f82dbcbd1fde3610b0b06ec096080d5b77cde3e2
Oleg Kasyanov <oleg@batumi.org>
2022-08-10 22:27:48 +0400

    Code optimization

--- src/MysqlTestCase.php
+++ src/MysqlTestCase.php
@@ -10,35 +10,20 @@ use PHPUnit\Framework\TestCase;
 
 class MysqlTestCase extends TestCase
 {
-    protected static string $dbHost;
-    protected static int $dbPort;
-    protected static string $dbName;
-    protected static string $dbUser;
-    protected static string $dbPass;
+    private static int $dbPort = 3306;
+    private static string $dbHost = 'localhost';
+    private static string $dbName = 'tmp_test_database';
+    private static string $dbUser = 'root';
+    private static string $dbPass = 'root';
 
     protected static PDO $pdo;
 
     public static function setUpBeforeClass(): void
     {
-        self::$dbHost = (string) (defined('TEST_DB_HOST') ? TEST_DB_HOST : 'localhost');
-        self::$dbPort = (int) (defined('TEST_DB_PORT') ? TEST_DB_PORT : 3306);
-        self::$dbUser = (string) (defined('TEST_DB_USER') ? TEST_DB_USER : 'root');
-        self::$dbPass = (string) (defined('TEST_DB_PASS') ? TEST_DB_PASS : 'root');
-        self::$dbName = (string) (defined('TEST_DB_NAME') ? TEST_DB_NAME : 'tmp_test_database');
-
-        self::$pdo = self::connect();
-    }
-
-    public static function tearDownAfterClass(): void
-    {
-        self::$pdo->exec('DROP DATABASE IF EXISTS ' . self::$dbName);
-    }
-
-    private static function connect(): PDO
-    {
         try {
-            $pdo = new PDO(
-                'mysql:host=' . self::$dbHost . ';port=' . self::$dbPort,
+            self::fetchConfig();
+            self::$pdo = new PDO(
+                sprintf('mysql:host=%s;port=%d;dbname=%s', self::$dbHost, self::$dbPort, self::$dbName),
                 self::$dbUser,
                 self::$dbPass,
                 [
@@ -46,15 +31,41 @@ class MysqlTestCase extends TestCase
                     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
                 ]
             );
-            $pdo->exec(
+            self::$pdo->exec(
                 'CREATE DATABASE IF NOT EXISTS ' . self::$dbName .
                 ' CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;' .
                 'USE ' . self::$dbName
             );
-
-            return $pdo;
         } catch (PDOException $e) {
             throw new \RuntimeException("\n\e[31m" . ' PDO EXCEPTION: ' . "\e[0m " . $e->getMessage() . "\n");
+        }
+    }
+
+    public static function tearDownAfterClass(): void
+    {
+        self::$pdo->exec('DROP DATABASE IF EXISTS ' . self::$dbName);
+    }
+
+    private static function fetchConfig()
+    {
+        if (defined('TEST_DB_HOST')) {
+            self::$dbHost = (string) TEST_DB_HOST;
+        }
+
+        if (defined('TEST_DB_PORT')) {
+            self::$dbPort = (int) TEST_DB_PORT;
+        }
+
+        if (defined('TEST_DB_NAME')) {
+            self::$dbName = (string) TEST_DB_NAME;
+        }
+
+        if (defined('TEST_DB_USER')) {
+            self::$dbUser = (string) TEST_DB_USER;
+        }
+
+        if (defined('TEST_DB_PASS')) {
+            self::$dbPass = (string) TEST_DB_PASS;
         }
     }
 }
