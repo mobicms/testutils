a4000142ac31ae809aef089ba91e634aac9cc2e2
Oleg Kasyanov <oleg@batumi.org>
2022-08-15 12:13:04 +0400

    Refactoring

--- src/MysqlTestCase.php
+++ src/MysqlTestCase.php
@@ -9,23 +9,25 @@ use PHPUnit\Framework\TestCase;
 
 class MysqlTestCase extends TestCase
 {
-    private static Config $config;
+    private static ConfigLoader $config;
     private static PDO $pdo;
 
     public static function setUpBeforeClass(): void
     {
-        self::$config = new Config();
+        $config = new ConfigLoader();
         $pdo = new PDO(
-            sprintf('mysql:host=%s;port=%d', self::$config->host(), self::$config->port()),
-            self::$config->user(),
-            self::$config->password(),
+            sprintf('mysql:host=%s;port=%d', $config->host(), $config->port()),
+            $config->user(),
+            $config->password(),
             [
                 PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
             ]
         );
-        $pdo->exec(sprintf('CREATE DATABASE IF NOT EXISTS %s CHARACTER SET utf8mb4', self::$config->dbName()));
-        $pdo->exec(sprintf('USE %s', self::$config->dbName()));
+        $pdo->exec(sprintf('CREATE DATABASE IF NOT EXISTS %s CHARACTER SET utf8mb4', $config->dbName()));
+        $pdo->exec(sprintf('USE %s', $config->dbName()));
+
+        self::$config = $config;
         self::$pdo = $pdo;
     }
 
