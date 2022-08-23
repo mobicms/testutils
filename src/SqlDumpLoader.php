b48f4da31dac4e45e5386d572b83432acd639589
Oleg Kasyanov <oleg@batumi.org>
2022-08-23 19:13:10 +0400

    Added ignoring tag

--- src/SqlDumpLoader.php
+++ src/SqlDumpLoader.php
@@ -40,7 +40,7 @@ class SqlDumpLoader
     private function splitSql(string $file): array
     {
         $query = trim(file_get_contents($file));
-        preg_match_all('/([^;]*?((\'.*?\')|(".*?"))?)*(;\s*|\s*$)/', $query, $matches);
+        preg_match_all('/([^;]*?((\'.*?\')|(".*?"))?)*?(;\s*|\s*$)/', $query, $matches); //NOSONAR
 
         return $matches[0];
     }
